02483d4d4be77d3dbe1655ae91ca8bef48d5a722
aungkomyint222 <118752609+aungkomyint222@users.noreply.github.com>
2025-02-07 20:11:21 +0700

    fdfsd
    df

--- app/layout.js
+++ app/layout.js
@@ -12,22 +12,22 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 export const metadata = {
-  title: 'KM Web Design - Creative Web Solutions',
+  title: 'KM Web Design -  Bangkok',
   description: 'KM Web Design offers professional web development, SEO optimization, and custom WordPress design services to help your business grow online.',
   openGraph: {
-    title: 'KM Web Design - Creative Web Solutions',
-    description: 'KM Web Design offers professional web development, SEO optimization, and custom WordPress design services.',
+    title: 'KM Web Design - Bangkok',
+    description: 'KM Web Design offers professional web development in Bangkok, SEO optimization, and custom WordPress design services.',
     url: 'https://kmwebdesign.netlify.com', // Update with your live site URL
     siteName: 'KM Web Design',
     locale: 'en_US',
     type: 'website',
-    image: 'https://kmwebdesign.netlify.com/images/og-image.jpg', // Update with an image URL for social sharing (make sure the image exists)
+    //image: 'https://kmwebdesign.netlify.com/images/og-image.jpg', // Update with an image URL for social sharing (make sure the image exists)
   },
   twitter: {
     card: 'summary_large_image',
-    title: 'KM Web Design - Creative Web Solutions',
+    title: 'KM Web Design - Bangkok ',
     description: 'KM Web Design offers professional web development and custom WordPress design services.',
-    image: 'https://kmwebdesign.netlify.com/images/og-image.jpg', // Update with the image URL
+   // image: 'https://kmwebdesign.netlify.com/images/og-image.jpg', // Update with the image URL
   },
   
   verification: {
@@ -40,8 +40,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-        <meta name="description" content="KM Web Design offers professional web development, SEO optimization, and custom WordPress design services to help your business grow online." />
-        <meta name="keywords" content="web design, WordPress, SEO, digital marketing, web development" />
+        <meta name="description" content="KM Web Design Bangkok offers professional web development, SEO optimization, and custom WordPress design services to help your business grow online." />
+        <meta name="keywords" content="web design, WordPress,Bangkok, SEO, digital marketing, web development" />
         <meta name="robots" content="index, follow" />
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
