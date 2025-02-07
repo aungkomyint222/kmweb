a072c8595c7dafb072238c9b3752858a40de15ec
aungkomyint222 <118752609+aungkomyint222@users.noreply.github.com>
2025-02-07 18:12:37 +0700

    dfd

--- app/layout.js
+++ app/layout.js
@@ -14,14 +14,16 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "KMWebDesign",
   description: "KMWebDesign is a web design and development agency.",
+  verification: {
+    google: "t9vpJzCd4NXXfGd2qiV3XzW9p-X5shEmIhOfW3xYq4k",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head />
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
